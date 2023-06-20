4e3e7ef164263f61619b7f62106f54423b78d8b2
unknown <juan32n@gmail.com>
2023-06-20 12:24:02 -0500

    first commit

--- var/cache/dev/twig/74/743a5d5ffe51c4bb1f0b7197b77ce9a7e947751fcd6a905b3871a44c3d4deabd.php
+++ var/cache/dev/twig/74/743a5d5ffe51c4bb1f0b7197b77ce9a7e947751fcd6a905b3871a44c3d4deabd.php
@@ -52,10 +52,7 @@ class __TwigTemplate_68acc643df30a393a6c723ce8de648a8534635e3cc1ca7db46bb4953ffc
         // line 10
         echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("resources/Inicio/inicioBundle/css/estilos.css"), "html", null, true);
         echo "\">
-\t\t<link rel=\"stylesheet\" href=\"";
-        // line 11
-        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("resources/Inicio/inicioBundle/js/js.js"), "html", null, true);
-        echo "\">
+\t\t<link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">
 
 \t\t<link
 \t\trel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css\">
@@ -75,72 +72,75 @@ class __TwigTemplate_68acc643df30a393a6c723ce8de648a8534635e3cc1ca7db46bb4953ffc
         echo "\" rel=\"stylesheet\"> ";
         $this->displayBlock('stylesheets', $context, $blocks);
         // line 23
-        echo "\t\t<link rel=\"icon\" type=\"image/x-icon\" href=\"";
+        echo "\t\t<link
+\t\trel=\"icon\" type=\"image/x-icon\" href=\"";
+        // line 24
         echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("favicon.ico"), "html", null, true);
         echo "\"/>
-\t<!-- Compiled and minified CSS -->
-\t\t";
-        // line 26
-        echo "
+\t\t<!-- Compiled and minified CSS -->
+\t\t<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css\">
+
 
 \t</head>
 \t<body>
 \t\t<!-- Menu Navigation -->
-\t\t<nav class=\"navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top\">
-\t\t\t<div class=\"container\">
-\t\t\t\t<a class=\"navbar-brand\" href=\" ";
-        // line 33
+\t\t<div class=\"nav-wrapper\">
+\t\t\t<nav class=\"navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top \">
+\t\t\t\t<div class=\"container col s12 m4 l2 nav-wrapper\">
+\t\t\t\t\t<a class=\"navbar-brand brand-logo\" href=\" ";
+        // line 35
         echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("inicioinicio_homepage");
         echo " \">Bar Juan</a>
-\t\t\t\t<button class=\"navbar-toggler navbar-toggler-right\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarResponsive\" aria-controls=\"navbarResponsive\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
-\t\t\t\t\t<span class=\"navbar-toggler-icon\"></span>
-\t\t\t\t</button>
-\t\t\t\t<div class=\"collapse navbar-collapse\" id=\"navbarResponsive\">
-\t\t\t\t\t<ul class=\"navbar-nav ml-auto\">
-\t\t\t\t\t\t<li class=\"nav-item\">
-\t\t\t\t\t\t\t<a class=\"nav-link\" href=\" ";
-        // line 40
+\t\t\t\t\t<button class=\"navbar-toggler navbar-toggler-right\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarResponsive\" aria-controls=\"navbarResponsive\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+\t\t\t\t\t\t<span class=\"navbar-toggler-icon\"></span>
+\t\t\t\t\t</button>
+\t\t\t\t\t<div class=\"collapse navbar-collapse\" id=\"navbarResponsive\">
+\t\t\t\t\t\t<ul class=\"navbar-nav ml-auto\">
+\t\t\t\t\t\t\t<li class=\"nav-item\">
+\t\t\t\t\t\t\t\t<a class=\"nav-link\" href=\" ";
+        // line 42
         echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("nosotros_i_inosotros_homepage");
         echo " \">Nosotros</a>
-\t\t\t\t\t\t</li>
-\t\t\t\t\t\t<li class=\"nav-item\">
-\t\t\t\t\t\t\t<a class=\"nav-link\" href=\" ";
-        // line 43
+\t\t\t\t\t\t\t</li>
+\t\t\t\t\t\t\t<li class=\"nav-item\">
+\t\t\t\t\t\t\t\t<a class=\"nav-link\" href=\" ";
+        // line 45
         echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("tapas_tapas_homepage");
         echo " \">Tapas</a>
-\t\t\t\t\t\t</li>
-\t\t\t\t\t\t<li class=\"nav-item dropdown\">
-\t\t\t\t\t\t\t<a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdownBlog\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
-\t\t\t\t\t\t\t\tBares
-\t\t\t\t\t\t\t</a>
-\t\t\t\t\t\t\t<div class=\"dropdown-menu dropdown-menu-right\" aria-labelledby=\"navbarDropdownBlog\">
-\t\t\t\t\t\t\t\t<a class=\"dropdown-item\" href=\"";
-        // line 50
+\t\t\t\t\t\t\t</li>
+\t\t\t\t\t\t\t<li class=\"nav-item dropdown\">
+\t\t\t\t\t\t\t\t<a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdownBlog\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+\t\t\t\t\t\t\t\t\tBares
+\t\t\t\t\t\t\t\t</a>
+\t\t\t\t\t\t\t\t<div class=\"dropdown-menu dropdown-menu-right\" aria-labelledby=\"navbarDropdownBlog\">
+\t\t\t\t\t\t\t\t\t<a class=\"dropdown-item\" href=\"";
+        // line 52
         echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("contactar_contactar_homepage", ["sitio" => "Celaya"]);
         echo "\">Celaya</a>
-\t\t\t\t\t\t\t\t<a class=\"dropdown-item\" href=\"";
-        // line 51
+\t\t\t\t\t\t\t\t\t<a class=\"dropdown-item\" href=\"";
+        // line 53
         echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("contactar_contactar_homepage", ["sitio" => "Comonfort"]);
         echo "\">comonfort</a>
-\t\t\t\t\t\t\t\t<a class=\"dropdown-item\" href=\"";
-        // line 52
+\t\t\t\t\t\t\t\t\t<a class=\"dropdown-item\" href=\"";
+        // line 54
         echo $this->env->getExtension('Symfony\Bridge\Twig\Extension\RoutingExtension')->getPath("contactar_contactar_homepage", ["sitio" => "Empalme_Escobedo"]);
         echo "\">Empalme Escobedo</a>
-\t\t\t\t\t\t\t</div>
-\t\t\t\t\t\t</li>
-\t\t\t\t\t\t<li class=\"nav-item\">
-\t\t\t\t\t\t\t<a class=\"nav-link\" href=\"#\">Reservas</a>
-\t\t\t\t\t\t</li>
+\t\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t</li>
+\t\t\t\t\t\t\t<li class=\"nav-item\">
+\t\t\t\t\t\t\t\t<a class=\"nav-link\" href=\"#\">Reservas</a>
+\t\t\t\t\t\t\t</li>
 
-\t\t\t\t\t</ul>
+\t\t\t\t\t\t</ul>
+\t\t\t\t\t</div>
 \t\t\t\t</div>
-\t\t\t</div>
-\t\t</nav>
+\t\t\t</nav>
+\t\t</div>
 \t\t<!-- Fin de Menu navigation -->
 \t\t";
-        // line 64
+        // line 67
         $this->displayBlock('contenido', $context, $blocks);
-        // line 65
+        // line 68
         echo "\t\t<!-- Footer -->
 \t\t<footer class=\"py-5 bg-dark\">
 \t\t\t<div class=\"container\">
@@ -149,9 +149,9 @@ class __TwigTemplate_68acc643df30a393a6c723ce8de648a8534635e3cc1ca7db46bb4953ffc
 \t\t\t<!-- /.container -->
 \t\t</footer>
 \t\t";
-        // line 72
+        // line 75
         $this->displayBlock('javascripts', $context, $blocks);
-        // line 82
+        // line 87
         echo "\t</body>
 </html>
 ";
@@ -199,7 +199,7 @@ class __TwigTemplate_68acc643df30a393a6c723ce8de648a8534635e3cc1ca7db46bb4953ffc
 
     }
 
-    // line 64
+    // line 67
     public function block_contenido($context, array $blocks = [])
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
@@ -216,7 +216,7 @@ class __TwigTemplate_68acc643df30a393a6c723ce8de648a8534635e3cc1ca7db46bb4953ffc
 
     }
 
-    // line 72
+    // line 75
     public function block_javascripts($context, array $blocks = [])
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
@@ -225,20 +225,24 @@ class __TwigTemplate_68acc643df30a393a6c723ce8de648a8534635e3cc1ca7db46bb4953ffc
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
 
-        // line 73
+        // line 76
         echo "\t\t\t<!-- boostrap core javaScript -->
 \t\t\t<script src=\"";
-        // line 74
+        // line 77
         echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("vendor/jquery/jquery.min.js"), "html", null, true);
         echo "\"></script>
 \t\t\t<script src=\"";
-        // line 75
+        // line 78
         echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("vendor/bootstrap/js/bootstrap.bundle.min.js"), "html", null, true);
         echo "\"></script>
+\t\t\t<script src=\"";
+        // line 79
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("resources/Inicio/inicioBundle/js/js.js"), "html", null, true);
+        echo "\"></script>
+
 \t\t\t<!-- Compiled and minified JavaScript -->
-\t\t\t";
-        // line 78
-        echo "\t\t\t<script src=\"https://kit.fontawesome.com/eb496ab1a0.js\" crossorigin=\"anonymous\"></script>
+\t\t\t<script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js\"></script>
+\t\t\t<script src=\"https://kit.fontawesome.com/eb496ab1a0.js\" crossorigin=\"anonymous\"></script>
 
 
 \t\t";
@@ -262,7 +266,7 @@ class __TwigTemplate_68acc643df30a393a6c723ce8de648a8534635e3cc1ca7db46bb4953ffc
 
     public function getDebugInfo()
     {
-        return array (  241 => 78,  236 => 75,  232 => 74,  229 => 73,  220 => 72,  203 => 64,  186 => 22,  167 => 6,  155 => 82,  153 => 72,  144 => 65,  142 => 64,  127 => 52,  123 => 51,  119 => 50,  109 => 43,  103 => 40,  93 => 33,  84 => 26,  78 => 23,  74 => 22,  69 => 20,  57 => 11,  53 => 10,  49 => 8,  47 => 6,  40 => 1,);
+        return array (  240 => 79,  236 => 78,  232 => 77,  229 => 76,  220 => 75,  203 => 67,  186 => 22,  167 => 6,  155 => 87,  153 => 75,  144 => 68,  142 => 67,  126 => 54,  122 => 53,  118 => 52,  108 => 45,  102 => 42,  92 => 35,  78 => 24,  75 => 23,  71 => 22,  66 => 20,  53 => 10,  49 => 8,  47 => 6,  40 => 1,);
     }
 
     /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
@@ -285,7 +289,7 @@ class __TwigTemplate_68acc643df30a393a6c723ce8de648a8534635e3cc1ca7db46bb4953ffc
 \t\t</title>
 \t\t<!-- css-->
 \t\t<link rel=\"stylesheet\" href=\"{{ asset('resources/Inicio/inicioBundle/css/estilos.css')}}\">
-\t\t<link rel=\"stylesheet\" href=\"{{ asset('resources/Inicio/inicioBundle/js/js.js')}}\">
+\t\t<link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">
 
 \t\t<link
 \t\trel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css\">
@@ -297,46 +301,49 @@ class __TwigTemplate_68acc643df30a393a6c723ce8de648a8534635e3cc1ca7db46bb4953ffc
 \t\thref=\"{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}\" rel=\"stylesheet\">
 \t\t<!-- Core theme CSS (includes Bootstrap)-->
 \t\t<link href=\"{{ asset('css/modern-business.css') }}\" rel=\"stylesheet\"> {% block stylesheets %}{% endblock %}
-\t\t<link rel=\"icon\" type=\"image/x-icon\" href=\"{{ asset('favicon.ico') }}\"/>
-\t<!-- Compiled and minified CSS -->
-\t\t{# <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css\"> #}
+\t\t<link
+\t\trel=\"icon\" type=\"image/x-icon\" href=\"{{ asset('favicon.ico') }}\"/>
+\t\t<!-- Compiled and minified CSS -->
+\t\t<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css\">
 
 
 \t</head>
 \t<body>
 \t\t<!-- Menu Navigation -->
-\t\t<nav class=\"navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top\">
-\t\t\t<div class=\"container\">
-\t\t\t\t<a class=\"navbar-brand\" href=\" {{path('inicioinicio_homepage')}} \">Bar Juan</a>
-\t\t\t\t<button class=\"navbar-toggler navbar-toggler-right\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarResponsive\" aria-controls=\"navbarResponsive\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
-\t\t\t\t\t<span class=\"navbar-toggler-icon\"></span>
-\t\t\t\t</button>
-\t\t\t\t<div class=\"collapse navbar-collapse\" id=\"navbarResponsive\">
-\t\t\t\t\t<ul class=\"navbar-nav ml-auto\">
-\t\t\t\t\t\t<li class=\"nav-item\">
-\t\t\t\t\t\t\t<a class=\"nav-link\" href=\" {{ path('nosotros_i_inosotros_homepage') }} \">Nosotros</a>
-\t\t\t\t\t\t</li>
-\t\t\t\t\t\t<li class=\"nav-item\">
-\t\t\t\t\t\t\t<a class=\"nav-link\" href=\" {{ path('tapas_tapas_homepage') }} \">Tapas</a>
-\t\t\t\t\t\t</li>
-\t\t\t\t\t\t<li class=\"nav-item dropdown\">
-\t\t\t\t\t\t\t<a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdownBlog\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
-\t\t\t\t\t\t\t\tBares
-\t\t\t\t\t\t\t</a>
-\t\t\t\t\t\t\t<div class=\"dropdown-menu dropdown-menu-right\" aria-labelledby=\"navbarDropdownBlog\">
-\t\t\t\t\t\t\t\t<a class=\"dropdown-item\" href=\"{{path('contactar_contactar_homepage',{'sitio':'Celaya'})}}\">Celaya</a>
-\t\t\t\t\t\t\t\t<a class=\"dropdown-item\" href=\"{{path('contactar_contactar_homepage',{'sitio':'Comonfort'})}}\">comonfort</a>
-\t\t\t\t\t\t\t\t<a class=\"dropdown-item\" href=\"{{path('contactar_contactar_homepage',{'sitio':'Empalme_Escobedo'})}}\">Empalme Escobedo</a>
-\t\t\t\t\t\t\t</div>
-\t\t\t\t\t\t</li>
-\t\t\t\t\t\t<li class=\"nav-item\">
-\t\t\t\t\t\t\t<a class=\"nav-link\" href=\"#\">Reservas</a>
-\t\t\t\t\t\t</li>
+\t\t<div class=\"nav-wrapper\">
+\t\t\t<nav class=\"navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top \">
+\t\t\t\t<div class=\"container col s12 m4 l2 nav-wrapper\">
+\t\t\t\t\t<a class=\"navbar-brand brand-logo\" href=\" {{path('inicioinicio_homepage')}} \">Bar Juan</a>
+\t\t\t\t\t<button class=\"navbar-toggler navbar-toggler-right\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarResponsive\" aria-controls=\"navbarResponsive\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+\t\t\t\t\t\t<span class=\"navbar-toggler-icon\"></span>
+\t\t\t\t\t</button>
+\t\t\t\t\t<div class=\"collapse navbar-collapse\" id=\"navbarResponsive\">
+\t\t\t\t\t\t<ul class=\"navbar-nav ml-auto\">
+\t\t\t\t\t\t\t<li class=\"nav-item\">
+\t\t\t\t\t\t\t\t<a class=\"nav-link\" href=\" {{ path('nosotros_i_inosotros_homepage') }} \">Nosotros</a>
+\t\t\t\t\t\t\t</li>
+\t\t\t\t\t\t\t<li class=\"nav-item\">
+\t\t\t\t\t\t\t\t<a class=\"nav-link\" href=\" {{ path('tapas_tapas_homepage') }} \">Tapas</a>
+\t\t\t\t\t\t\t</li>
+\t\t\t\t\t\t\t<li class=\"nav-item dropdown\">
+\t\t\t\t\t\t\t\t<a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdownBlog\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+\t\t\t\t\t\t\t\t\tBares
+\t\t\t\t\t\t\t\t</a>
+\t\t\t\t\t\t\t\t<div class=\"dropdown-menu dropdown-menu-right\" aria-labelledby=\"navbarDropdownBlog\">
+\t\t\t\t\t\t\t\t\t<a class=\"dropdown-item\" href=\"{{path('contactar_contactar_homepage',{'sitio':'Celaya'})}}\">Celaya</a>
+\t\t\t\t\t\t\t\t\t<a class=\"dropdown-item\" href=\"{{path('contactar_contactar_homepage',{'sitio':'Comonfort'})}}\">comonfort</a>
+\t\t\t\t\t\t\t\t\t<a class=\"dropdown-item\" href=\"{{path('contactar_contactar_homepage',{'sitio':'Empalme_Escobedo'})}}\">Empalme Escobedo</a>
+\t\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t\t</li>
+\t\t\t\t\t\t\t<li class=\"nav-item\">
+\t\t\t\t\t\t\t\t<a class=\"nav-link\" href=\"#\">Reservas</a>
+\t\t\t\t\t\t\t</li>
 
-\t\t\t\t\t</ul>
+\t\t\t\t\t\t</ul>
+\t\t\t\t\t</div>
 \t\t\t\t</div>
-\t\t\t</div>
-\t\t</nav>
+\t\t\t</nav>
+\t\t</div>
 \t\t<!-- Fin de Menu navigation -->
 \t\t{% block contenido %}{% endblock %}
 \t\t<!-- Footer -->
@@ -350,8 +357,10 @@ class __TwigTemplate_68acc643df30a393a6c723ce8de648a8534635e3cc1ca7db46bb4953ffc
 \t\t\t<!-- boostrap core javaScript -->
 \t\t\t<script src=\"{{asset('vendor/jquery/jquery.min.js') }}\"></script>
 \t\t\t<script src=\"{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}\"></script>
+\t\t\t<script src=\"{{ asset('resources/Inicio/inicioBundle/js/js.js')}}\"></script>
+
 \t\t\t<!-- Compiled and minified JavaScript -->
-\t\t\t{# <script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js\"></script> #}
+\t\t\t<script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js\"></script>
 \t\t\t<script src=\"https://kit.fontawesome.com/eb496ab1a0.js\" crossorigin=\"anonymous\"></script>
 
 
--- web/resources/Inicio/inicioBundle/js/js.js
+++ web/resources/Inicio/inicioBundle/js/js.js
@@ -4,18 +4,18 @@ const btnTapaNueva = document.querySelector("#btnModal");
 /* cancelacion Modal*/
 
 $("#btnModal").on("click", function () {
-    console.log("llega hasta aqui")
+    // console.log("llega hasta aqui")
     $("#Tapa").modal({ dismissible: false }).modal('open');
 });
 
 //cierra el modal y lo vacía
 $(".close-modal-Tapa").on("click", function () {
     // $("#data").empty();
-    console.log("llega hasta aqui")
+    // console.log("llega hasta aqui")
     $("#Tapa").modal("close");
 });
 
-$('.carousel.carousel-slider').carousel({fullWidth: true});
+// $('.carousel.carousel-slider').carousel({fullWidth: true});
 
 
 $(document).ready(function(){
